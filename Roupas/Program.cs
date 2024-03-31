f2f0d73efabe61039254ea9015fcd84cf3d51046
FelipeJaniro <164914243+FelipeJaniro@users.noreply.github.com>
2024-03-30 21:39:33 -0300

    Add files via upload

---
+++ Roupas/Program.cs
@@ -0,0 +1,12 @@
+﻿public class roupas{
+    public string Color {get;set;}
+
+    public string Fabric {get;set;}
+
+    public string Brand {get;set;}
+
+    public string Model {get;set;}
+
+    public string Size {get;set;}
+
+}
